72e60a5e91ced18f067f142f5c9840f459e34c75
simonh1005 <simon.hirschmann@yahoo.de>
2016-03-16 20:12:12 +0100

    pseudo Answer for ressource auth

--- drkUniProjekt/src/rest/AuthRes.java
+++ drkUniProjekt/src/rest/AuthRes.java
@@ -2,9 +2,12 @@ package rest;
 
 public class AuthRes extends Ressource
 {
-	public void doPost()
+	@Override
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
 	{
-		
+		response.getWriter().write("{\"token\":\"bearer 12345678910\"}");
+		response.getWriter().flush();
+		response.getWriter().close();		
 	}
 
 }
