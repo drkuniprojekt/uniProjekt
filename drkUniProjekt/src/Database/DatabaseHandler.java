c4ba50838b26333b788240702c2783b3325364d3
simonh1005 <simon.hirschmann@yahoo.de>
2016-03-17 19:10:35 +0100

    dbhandler

--- drkUniProjekt/src/Database/DatabaseHandler.java
+++ drkUniProjekt/src/Database/DatabaseHandler.java
@@ -3,6 +3,7 @@ package Database;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
@@ -14,6 +15,8 @@ import javax.naming.InitialContext;
 import javax.naming.NamingEnumeration;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
+
+import org.json.simple.JSONObject;
 
 public class DatabaseHandler
 {
@@ -90,6 +93,71 @@ public class DatabaseHandler
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		return tmp;
+	}
+	public JSONObject JsonTest()
+	{
+		try 
+		{
+			return executeQuery("SELECT * FROM TESTTABELLE");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			JSONObject j	= new JSONObject();
+			j.put("Error", e);
+			return j;
+		}
+		
+	}
+	/**
+	 * 
+	 * @param Query The Query to execute
+	 * @return An JSONObject containing thhe result
+	 * @throws SQLException 
+	 */
+	public JSONObject executeQuery(String query) throws SQLException
+	{
+		Statement stmt 	= conn.createStatement();
+		ResultSet rs 	= stmt.executeQuery(query);	
+		return rsToJSON(rs);				
+		
+	}
+
+	private JSONObject rsToJSON(ResultSet rs) throws SQLException {
+		JSONObject tmp	= new JSONObject();
+		ResultSetMetaData rsmd = rs.getMetaData();
+	    int columnCount = rsmd.getColumnCount();
+	    String column;    
+		while (rs.next())
+		{
+			for (int index = 1; index <= columnCount; index++) {
+	            column = rsmd.getColumnName(1);
+	            Object value = rs.getObject(column);
+//	            if (value == null) {
+//	                tmp.put(column, null);
+//	            } else if (value instanceof Integer) {
+//	                tmp.put(column, (Integer) value);
+//	            } else if (value instanceof String) {
+//	                tmp.put(column, (String) value);                
+//	            } else if (value instanceof Boolean) {
+//	                tmp.put(column, (Boolean) value);           
+//	            } else if (value instanceof Date) {
+//	                tmp.put(column, ((Date) value).getTime());                
+//	            } else if (value instanceof Long) {
+//	                tmp.put(column, (Long) value);                
+//	            } else if (value instanceof Double) {
+//	                tmp.put(column, (Double) value);                
+//	            } else if (value instanceof Float) {
+//	                tmp.put(column, (Float) value);                
+//	            } else if (value instanceof Byte) {
+//	                tmp.put(column, (Byte) value);
+//	            } else if (value instanceof byte[]) {
+//	                tmp.put(column, (byte[]) value);                
+//	            } else {
+	            	tmp.put(column, value);
+//	            }
+			}
+		}
+		System.out.println("Read from Dataabase" + tmp);			
 		return tmp;
 	}
 }
