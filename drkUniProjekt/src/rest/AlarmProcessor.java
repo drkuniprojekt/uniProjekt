359f6f3f420ace221e7e66b66fad16118cca535a
FlorianSchrage <florian.schrage@sap.com>
2016-03-19 17:28:17 +0100

    Alarm Get

---
+++ drkUniProjekt/src/rest/AlarmProcessor.java
@@ -0,0 +1,51 @@
+package rest;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.simple.JSONObject;
+
+@WebServlet("/alarm/*")
+public class AlarmProcessor extends HttpServlet
+{
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+			try
+			{
+				Alarm alarm = new Alarm();
+				JSONObject responseJSON = alarm.getJSON();
+				if(responseJSON.isEmpty())
+					response.setStatus(HttpServletResponse.SC_NO_CONTENT);
+				Helper.setResponseJSON(response, responseJSON);
+			} catch (IllegalStateException e)
+			{
+				e.printStackTrace();
+				response.sendError(HttpServletResponse.SC_CONFLICT);
+			} catch (SQLException e)
+			{
+				e.printStackTrace();
+				response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+			}	
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		
+	}
+	
+	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		
+	}
+	
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		
+	}
+}
