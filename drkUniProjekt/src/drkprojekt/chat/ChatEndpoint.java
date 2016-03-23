18190fd2e8a55bad5ad5f99d26a5941ac933ce73
Simon H <simon.hirschmann@yahoo.de>
2016-03-23 11:45:44 +0100

    ChatEnpoint Test

--- drkUniProjekt/src/drkprojekt/chat/ChatEndpoint.java
+++ drkUniProjekt/src/drkprojekt/chat/ChatEndpoint.java
@@ -1,28 +1,40 @@
 package drkprojekt.chat;
 
+import java.sql.SQLException;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
 import javax.websocket.*;
 import javax.websocket.server.ServerEndpoint;
+
 import org.slf4j.Logger; 
 import org.slf4j.LoggerFactory;
+
+import drkprojekt.rest.PushService;
 
 @ServerEndpoint("/chat")
 public class ChatEndpoint
 {
 	private static Set<Session> peers	= Collections.synchronizedSet(new HashSet<Session>());
-	
+	private static Logger log;
 	@OnOpen
 	public void onOpen(Session peer)
 	{
-		peers.add(peer);		
+		peers.add(peer);
+		log	= LoggerFactory.getLogger(this.getClass());
 	}
 	
 	@OnMessage
 	public void onMessage(String msg)
 	{
-		
+		try
+		{
+			PushService.sendBroadCastMessage(msg);
+			log.info("Got new Message: " + msg);
+		} catch (SQLException e)
+		{
+			e.printStackTrace();
+		}
 	}
 }
