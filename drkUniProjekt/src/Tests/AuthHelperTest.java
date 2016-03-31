21d962bd6c312198921b7ff73b7e2e5c4acc665b
Steffen Terheiden <Steffen Terheiden@SOBIS-089-NBK.sobis.com>
2016-03-31 10:16:45 +0200

    Test

--- drkUniProjekt/src/Tests/AuthHelperTest.java
+++ drkUniProjekt/src/Tests/AuthHelperTest.java
@@ -8,6 +8,7 @@ import static org.junit.Assert.*;
 import java.security.SignatureException;
 import java.util.Calendar;
 
+import org.joda.time.DateTime;
 import org.junit.Test;
 
 import drkprojekt.auth.AuthHelper;
@@ -21,24 +22,29 @@ public class AuthHelperTest {
 
     @Test
     public void test() {
-	String token="";
-	try {
-	    token = AuthHelper.createJsonWebToken("User1", "Susi Sorglos", false, (long)10000);
-	} catch (SignatureException e) {
-	    // TODO Auto-generated catch block
-	    e.printStackTrace();
-	}
-	System.out.println(token);
-	//token="eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJEUktVbmlQcm9qZWt0IiwiYXVkIjoiQ2xpZW50Rm9yRFJLTWVtYmVyIiwiaWF0IjoxNDU4NzQ3NTM0LCJleHAiOjIzMjI3NDc1MzQsImluZm8iOnsidXNlcklkIjoiVXNlcjEiLCJ1c2VyUm9sZSI6InRlYW1NZW1iZXIifX0.nsp9FGp7oOn1SQBXnFv-un7s3ePEI0zIx9FYDsaMINs";
-	TokenInfo info = AuthHelper.verifyToken(token);
-	System.out.println("USer:" +info.getUserId());
-	if(info.getExpires().getMillis()<System.currentTimeMillis()/1000){
-	    System.out.println("Expired");
-	}
-	System.out.println(System.currentTimeMillis());
-	System.out.println(info.getIssued().getMillis());
-	System.out.println(info.getExpires().getMillis());
-	assertEquals("Userid gleich","User1",info.getUserId());
+//	String token="";
+//	try {
+//	    token = AuthHelper.createJsonWebToken("User1", "Susi Sorglos", false, (long)10000);
+//	} catch (SignatureException e) {
+//	    // TODO Auto-generated catch block
+//	    e.printStackTrace();
+//	}
+//	System.out.println(token);
+//	//token="eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJEUktVbmlQcm9qZWt0IiwiYXVkIjoiQ2xpZW50Rm9yRFJLTWVtYmVyIiwiaWF0IjoxNDU4NzQ3NTM0LCJleHAiOjIzMjI3NDc1MzQsImluZm8iOnsidXNlcklkIjoiVXNlcjEiLCJ1c2VyUm9sZSI6InRlYW1NZW1iZXIifX0.nsp9FGp7oOn1SQBXnFv-un7s3ePEI0zIx9FYDsaMINs";
+//	TokenInfo info = AuthHelper.verifyToken(token);
+//	System.out.println("USer:" +info.getUserId());
+//	if(info.getExpires().getMillis()<System.currentTimeMillis()/1000){
+//	    System.out.println("Expired");
+//	}
+//	System.out.println(System.currentTimeMillis());
+//	System.out.println(info.getIssued().getMillis());
+//	System.out.println(info.getExpires().getMillis());
+//	assertEquals("Userid gleich","User1",info.getUserId());
+	Calendar cal = Calendar.getInstance();   
+	System.out.println(new org.joda.time.Instant(cal.getTimeInMillis()));
+	System.out.println(new org.joda.time.Instant(cal.getTimeInMillis() + 1000L * 60L * 60L * 24L * 10000));
+	
+	
     }
 
 }
