5c09eeee7aee27c89f262bb42a3c715f7c3b3272
Steffen Terheiden <Steffen Terheiden@SOBIS-089-NBK.sobis.com>
2016-04-19 16:59:02 +0200

    Vllt doch ein Servlet :D

--- drkUniProjekt/src/drkprojekt/rest/UnreadMessagesProcessor.java
+++ drkUniProjekt/src/drkprojekt/rest/UnreadMessagesProcessor.java
@@ -8,6 +8,7 @@ import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -22,7 +23,7 @@ import drkprojekt.database.DatabaseHandler;
  *
  */
 @WebServlet("/unreadMessages/*")
-public class UnreadMessagesProcessor {
+public class UnreadMessagesProcessor extends HttpServlet{
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
